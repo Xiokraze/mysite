39d443d4076a5c6a2d87e8a545b93ed2b41cbaea
Joshua Worthington <Xiokraze@gmail.com>
2021-08-15 12:41:08 -0500

    Initial commit.

---
+++ mysite/src/App.js
@@ -0,0 +1,12 @@
+import { Switch, Route } from "react-router-dom";
+import React, { Suspense } from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <p>hi</p>
+    </React.Fragment>
+  );
+}
+
+export default App;

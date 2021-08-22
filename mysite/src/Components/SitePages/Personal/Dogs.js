94975c714e5ed0001a7919628db4ce64b824d87c
Joshua Worthington <Xiokraze@gmail.com>
2021-08-22 00:15:39 -0500

    Added dog photos and image carousel component.

--- mysite/src/Components/SitePages/Personal/Dogs.js
+++ mysite/src/Components/SitePages/Personal/Dogs.js
@@ -1,14 +1,27 @@
 import XiaHeader from "../../../Assets/Dogs/x1.jpg";
+import x2 from "../../../Assets/Dogs/x2.jpg";
+import x3 from "../../../Assets/Dogs/x3.jpg";
+import x4 from "../../../Assets/Dogs/x4.jpg";
+import x5 from "../../../Assets/Dogs/x5.jpg";
+import x6 from "../../../Assets/Dogs/x6.jpg";
+import x7 from "../../../Assets/Dogs/x7.jpg";
+import x8 from "../../../Assets/Dogs/x8.jpg";
 import NixieHeader from "../../../Assets/Dogs/n1.jpg";
+import n2 from "../../../Assets/Dogs/n2.jpg";
+import n3 from "../../../Assets/Dogs/n3.jpg";
+import n4 from "../../../Assets/Dogs/n4.jpg";
 import styles from "./Dogs.module.css";
 import React, { useState } from "react";
+import ImageGallery from "../../UIElements/ImageGallery";
 
 const Dogs = () => {
+  const xiaImages = [XiaHeader, x2, x3, x4, x5, x6, x7, x8];
+  const nixieImages = [NixieHeader, n2, n3, n4];
+
   return (
     <div className={styles.rootDogsDiv}>
       <div className={styles.headerImgContainer}>
-        <h2>Xia</h2>
-        <img src={XiaHeader} alt="" className={styles.headerImage} />
+        <ImageGallery title="Xia" images={xiaImages} />
         <h2>Breed</h2>
         <p>Miniature Australian Shephard</p>
         <h2>Aliases</h2>
@@ -23,8 +36,7 @@ const Dogs = () => {
         <p>Lamb Chop or Monster</p>
       </div>
       <div className={styles.headerImgContainer}>
-        <h2>Nixie</h2>
-        <img src={NixieHeader} alt="" className={styles.headerImage} />
+        <ImageGallery title="Nixie" images={nixieImages} />
         <h2>Breed</h2>
         <p>Toy Australian Shephard</p>
         <h2>Aliases</h2>
---
+++ mysite/src/Components/UIElements/ImageGallery.js
@@ -0,0 +1,20 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import styles from "./ImageGallery.module.css";
+
+// https://www.npmjs.com/package/react-responsive-carousel
+
+const ImageGallery = ({ title, images }) => {
+  return (
+    <div className={styles.imgRootDiv}>
+      <h2>{title}</h2>
+      <Carousel autoPlay interval="5000" transitionTime="2000" infiniteLoop="true">
+        {images.map((image) => (
+          <img key={image} src={image} className={styles.img}/>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default ImageGallery;
